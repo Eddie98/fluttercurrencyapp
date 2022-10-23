13dab551c35f4bd03380f49f7e4e13039fce33ce
Eddie98 <eddie.morra1234@gmail.com>
2022-10-23 16:09:21 +0600

    little code fix

--- lib/ui/pages/home/tabs/home/home.dart
+++ lib/ui/pages/home/tabs/home/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:testproject/constants/constants.dart';
 
+import '../../../../../constants/colors.dart';
+import '../../../../../utils/size_config.dart';
 import 'bloc/home_bloc.dart';
 
 class HomeTab extends StatelessWidget {
@@ -20,22 +22,51 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    return LayoutBuilder(builder: (context, constraints) {
+      return RefreshIndicator(
+        color: Colors.white,
+        backgroundColor: Colours.themeColor,
+        onRefresh: () async {
+          context.read<HomeBloc>().add(
+                HomeLoadInitialDataEvent(
+                  currencyPairs: testDefaultCurrencyPairs,
+                  fromToMap: testDefaultFromToMap,
+                  goAuth: goAuth,
+                  showSnackbar: snackbar,
+                ),
+              );
+        },
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(
+            horizontal: getPropScreenWidth(defaultHorPadding),
+          ),
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: SizedBox(
+            height: constraints.maxHeight,
+            child: BlocBuilder<HomeBloc, HomeState>(
+              builder: (context, state) {
+                if (state is HomeLoadingState) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (state is HomeErrorState) {
+                  return const Center(child: Text(someWentWrong));
+                }
+                if (state is HomeLoadedState) {
+                  final signals = state.partnerAnalyticSignals;
 
-    return BlocBuilder<HomeBloc, HomeState>(
-      builder: (context, state) {
-        if (state is HomeLoadingState) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        if (state is HomeErrorState) {
-          return const Center(child: Text(someWentWrong));
-        }
-        if (state is HomeLoadedState) {
-          return Center(
-              child: Text(state.partnerAnalyticSignals.last.pair.toString()));
-        }
-        return const SizedBox.shrink();
-      },
-    );
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      sizedBoxHeight(22.0),
+                    ],
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
